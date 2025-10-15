270675ad1d0475b21635c53d096c8d3ad301f671
AppTech_IT <morsun1995@gmail.com>
2025-10-15 11:26:51 +0600

    secent

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Hello Serverless Express!");
 });
+app.get("/home", (req, res) => {
+  res.send("Hello Serverless less Express!");
+});
 
 module.exports = app; // Export করব যাতে serverless handle করতে পারে
 // module.exports.handler = require("serverless-http")(app);
